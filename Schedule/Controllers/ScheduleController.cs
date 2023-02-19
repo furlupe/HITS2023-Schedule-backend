0b2fc553f9fc8ebbcd2f1f5607884272ac71beb1
Ilia Volgin <228133713372282004@stud.tsu.ru>
2023-02-19 17:24:19 +0700

    Described controller for "schedule-getters" "Schedule-getters" are those one, that return group/teacher/classroom schedule from date to date

---
+++ Schedule/Controllers/ScheduleController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Schedule.Controllers
+{
+    [ApiController]
+    [Route("api/schedule")]
+    public class ScheduleController : ControllerBase
+    {
+        [HttpGet("group/{id}")]
+        public IActionResult GetGroupSchedule(
+            [BindRequired] Guid id, 
+            [BindRequired] DateTime startsAt, 
+            [BindRequired] DateTime endsAt)
+        {
+            return Ok();
+        }
+
+        [HttpGet("teacher/{id}")]
+        public IActionResult GetTeacherSchedule(
+            [BindRequired] Guid id,
+            [BindRequired] DateTime startsAt,
+            [BindRequired] DateTime endsAt)
+        { 
+            return Ok(); 
+        }
+
+        [HttpGet("classroom/{number}")]
+        public IActionResult GetClassroomSchedule(
+            [BindRequired] int number,
+            [BindRequired] DateTime startsAt,
+            [BindRequired] DateTime endsAt)
+        {
+            return Ok();
+        }
+    }
+}
