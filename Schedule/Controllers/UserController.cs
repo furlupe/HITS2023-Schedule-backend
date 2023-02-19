3141b69306c51fd7b70abc6724c8b3aede54fdd4
Ilia Volgin <228133713372282004@stud.tsu.ru>
2023-02-19 22:45:34 +0700

    Described endpoint for getting user's info

--- Schedule/Controllers/UserController.cs
+++ Schedule/Controllers/UserController.cs
@@ -4,9 +4,15 @@ using Microsoft.AspNetCore.Mvc.ModelBinding;
 namespace Schedule.Controllers
 {
     [ApiController]
-    [Route("account")]
+    [Route("user")]
     public class UserController : ControllerBase
     {
+        [HttpGet("{id}")]
+        public IActionResult GetUser([BindRequired] Guid id)
+        {
+            return Ok();
+        }
+
         // TODO: create DTO for user changed info
         [HttpPut("{id}")]
         public IActionResult UpdateUser([BindRequired] Guid id)
