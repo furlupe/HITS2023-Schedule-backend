3fdba56bd0f90eb25cf1e3ee7fcacafeb68fdae6
Ilia Volgin <228133713372282004@stud.tsu.ru>
2023-03-08 23:52:15 +0700

    Made API to take and return enum values as strings

--- Schedule/Enums/LessonType.cs
+++ Schedule/Enums/LessonType.cs
@@ -2,14 +2,14 @@
 {
     public enum LessonType
     {
-        LECTURE = 0,
-        SEMINAR = 1,
-        PRACTICE = 2,
-        LABWORK = 3,
-        INDIVIDUAL_LESSON = 4,
-        CONTROL_POINT = 5,
-        CONSULTATION = 6,
-        RESERVATION = 7,
-        OTHER = 8
+        LECTURE,
+        SEMINAR,
+        PRACTICE,
+        LABWORK,
+        INDIVIDUAL_LESSON,
+        CONTROL_POINT,
+        CONSULTATION,
+        RESERVATION,
+        OTHER,
     }
 }
